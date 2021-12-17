5e8c57d7fa7feb90ff73b50d8f9c732957724e33
sravankumarrapolu143 <74374630+sravankumarrapolu143@users.noreply.github.com>
2021-12-17 09:25:37 -0500

    update main activity

--- app/src/main/java/com/example/getjob_andriod/MainActivity.java
+++ app/src/main/java/com/example/getjob_andriod/MainActivity.java
@@ -17,16 +17,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
-
-        topanim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
-        bottomanim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
-
-        image = findViewById(R.id.imageView);
-        logo = findViewById(R.id.textView);
-
-        image.setAnimation(topanim);
-        logo.setAnimation(bottomanim);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.postjob_screen);
+//
+//        topanim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+//        bottomanim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+//
+//        image = findViewById(R.id.imageView);
+//        logo = findViewById(R.id.textView);
+//
+//        image.setAnimation(topanim);
+//        logo.setAnimation(bottomanim);
     }
 }
