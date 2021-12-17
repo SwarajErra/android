ba53a4521f55fa9cc9939aac9819f55bb0ab35a4
krinesh2598 <74370237+krinesh2598@users.noreply.github.com>
2021-12-16 22:55:38 -0500

    pages
    new pages
    
    Co-Authored-By: Tejveer2608 <92998539+Tejveer2608@users.noreply.github.com>

--- app/src/main/java/com/example/getjob_andriod/MainActivity.java
+++ app/src/main/java/com/example/getjob_andriod/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.getjob_andriod;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+<<<<<<< Updated upstream
+    Animation topanim,bottomanim;
+    ImageView image;
+    TextView logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+=======
+Animation topanim,bottomanim;
+ImageView image;
+TextView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+>>>>>>> Stashed changes
         setContentView(R.layout.activity_main);
+
+        topanim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomanim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+
+        image.setAnimation(topanim);
+        logo.setAnimation(bottomanim);
+<<<<<<< Updated upstream
+
+=======
+>>>>>>> Stashed changes
     }
 }
