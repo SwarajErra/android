03b5103a70e83ea356fa584c0d7015a082bed218
krinesh2598 <74370237+krinesh2598@users.noreply.github.com>
2022-01-05 21:09:36 -0500

    updated pages

--- app/src/main/java/com/example/getjob_andriod/MainActivity.java
+++ app/src/main/java/com/example/getjob_andriod/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,6 +16,9 @@ import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 5000;
+
     Animation topanim,bottomanim;
     ImageView image;
     TextView logo;
@@ -21,30 +27,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        addListenerOnButton();
 
-    }
-    public void addListenerOnButton() {
+      //animations
+      topanim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+      bottomanim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
 
-        final Context context = this;
+      //hook
+      image = findViewById(R.id.imageView);
+      logo = findViewById(R.id.textView);
 
-//        button = (Button) findViewById(R.id.button1);
+      image.setAnimation(topanim);
+      logo.setAnimation(bottomanim);
 
 
-        textView = (TextView) findViewById(R.id.textView);
-        textView.setOnClickListener(new OnClickListener() {
+      new Handler().postDelayed(new Runnable() {
+          @Override
+          public void run() {
+              Intent intent = new Intent(MainActivity.this,loginActivity.class);
+              startActivity(intent);
+              finish();
+          }
+      },SPLASH_TIME);
 
-            @Override
-            public void onClick(View arg0) {
-
-                Intent intent = new Intent(context, loginActivity.class);
-                startActivity(intent);
-
-            }
-
-        });
-
-    }
-
-}
+    }}
